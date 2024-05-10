96204f5ff5037ed75edd2bf882ddc0df9dc576a1
Andrei Luciano Gomes <andrei_lucianoc@hotmail.com>
2024-05-10 15:06:59 -0300

    criando interfaces e configurando módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,11 +22,14 @@ import { MomentFormComponent } from './components/moment-form/moment-form.compon
     HomeComponent,
     NewMomentComponent,
     MomentFormComponent
-   
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
